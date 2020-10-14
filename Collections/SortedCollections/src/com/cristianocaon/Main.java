f95c3601fb236a4eccaea367fd0b566a372f9f28
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-14 10:25:49 -0500

    fix: basket toString | main updated

--- Collections/SortedCollections/src/com/cristianocaon/Main.java
+++ Collections/SortedCollections/src/com/cristianocaon/Main.java
@@ -19,6 +19,9 @@ public class Main {
         temp = new StockItem("Cup", 0.5, 200);
         stockList.addStock(temp);
 
+        temp = new StockItem("Cup", 0.45, 7);
+        stockList.addStock(temp);
+
         temp = new StockItem("Door", 72.95, 4);
         stockList.addStock(temp);
 
@@ -34,7 +37,45 @@ public class Main {
         temp = new StockItem("Vase", 8.76, 40);
         stockList.addStock(temp);
 
+//        for (String s : stockList.Items().keySet()) {
+//            System.out.println(s);
+//        }
+
+        Basket timsBasket = new Basket("Tim");
+        sellItem(timsBasket, "Car", 1);
+        System.out.println(timsBasket);
+
+        sellItem(timsBasket, "Car", 1);
+        System.out.println(timsBasket);
+
+        if (sellItem(timsBasket, "Car", 1) != 1) {
+            System.out.println("There are no more cars in stock");
+        }
+        sellItem(timsBasket, "Spanner", 5);
+        System.out.println(timsBasket);
+
+        sellItem(timsBasket, "Juice", 4);
+
+        sellItem(timsBasket, "Cup", 12);
+
+        sellItem(timsBasket, "Bread", 1);
+
+        System.out.println(timsBasket);
 
         System.out.println(stockList);
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        // Retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null) {
+            System.out.println("\nWe don't sell " + item);
+            return 0;
+        }
+        if (stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
     }
 }
