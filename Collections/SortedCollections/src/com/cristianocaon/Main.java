194a332e4cf3b66406fd1e0a9e41950da6b7df5f
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-14 09:31:47 -0500

    StockItems added

--- Collections/SortedCollections/src/com/cristianocaon/Main.java
+++ Collections/SortedCollections/src/com/cristianocaon/Main.java
@@ -1,8 +1,40 @@
 package com.cristianocaon;
 
 public class Main {
+    private static StockList stockList = new StockList();
 
     public static void main(String[] args) {
+        StockItem temp = new StockItem("Bread", 0.86, 100);
+        stockList.addStock(temp);
 
+        temp = new StockItem("Cake", 1.10, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Car", 12.5, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Chair", 52.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Cup", 0.5, 200);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Juice", 2.5, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Towel", 2.4, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Vase", 8.76, 40);
+        stockList.addStock(temp);
+
+
+        System.out.println(stockList);
     }
 }
