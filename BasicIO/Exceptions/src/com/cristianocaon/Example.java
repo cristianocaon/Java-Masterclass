f3b33730798a408a84ba1733f2288c5cbeef866c
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-17 10:51:27 -0500

    exception handling

--- BasicIO/Exceptions/src/com/cristianocaon/Example.java
+++ BasicIO/Exceptions/src/com/cristianocaon/Example.java
@@ -1,24 +1,45 @@
 package com.cristianocaon;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide() {
-        int x = getInt();
-        int y = getInt();
+        int x, y;
+//        try {
+        x = getInt();
+        y = getInt();
         System.out.println("X is " + x + ". Y is " + y);
         return x / y;
+//        } catch (NoSuchElementException e) {
+//            throw new NoSuchElementException("No suitable input");
+//        } catch (ArithmeticException e) {
+//            throw new ArithmeticException("Attempt to divide by zero");
+//        }
     }
 
     private static int getInt() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter an integer: ");
-        return scanner.nextInt();
+        while(true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.print("Please, enter a number (using digits 0-9): ");
+            }
+        }
     }
 }
